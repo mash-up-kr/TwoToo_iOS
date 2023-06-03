55b76999d6827d96ca87e1a6f9378e0b01d91fae
Ethen <ethen@spoqa.com>
2023-06-03 14:19:49 +0900

    🎉 프로젝트 시작

---
+++ TwoToo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoToo
+//
+//  Created by 박건우 on 2023/06/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
