9593ec5395f61ba01b06b7108b5f132fa32a12ed
kimkyunghun3 <kh.kim@lifesemantics.kr>
2023-06-09 23:35:29 +0900

    ✨ Utils 관련 메서드 구현

---
+++ Core/Util/Sources/Util/SceneUtils/Optional+.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  
+//
+//  Created by Eddy on 2023/06/09.
+//
+
+import Foundation
+
+/*
+ 옵셔널을 제거하고 이어지는 행동을 작성한다.
+
+ title.unwrap { value in
+     print(value)
+ }
+
+*/
+public extension Optional {
+    func unwrap(_ completion: @escaping (Wrapped) -> Void) {
+        if let unwrappedValue = self {
+            completion(unwrappedValue)
+        }
+    }
+}
