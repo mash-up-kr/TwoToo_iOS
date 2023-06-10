e5c62521644bde4804a53e9dfc06a43639db13c4
julia0926 <kj980926@naver.com>
2023-06-10 14:37:57 +0900

    ✨DesignSystem Color 적용

---
+++ Core/DesignSystem/Sources/DesignSystem/Sources/Color/Pallete.swift
@@ -0,0 +1,36 @@
+//
+//  File.swift
+//  
+//
+//  Created by Julia on 2023/06/10.
+//
+
+import UIKit
+
+public enum Pallete: String {
+    
+    case primary = "Primary"
+    
+    // MARK: - Main Color
+    case mainCoral = "Main_Coral"
+    case mainLightPink = "Main_LightPink"
+    case mainPink = "Main_Pink"
+    case mainWhite = "Main_White"
+    
+    // MARK: - Secondary Color
+    case second01 = "Second_01"
+    case second02 = "Second_02"
+    
+    // MARK: - Gray Color
+    case grey200 = "Grey_200"
+    case grey300 = "Grey_300"
+    case grey400 = "Grey_400"
+    case grey500 = "Grey_500"
+    case grey600 = "Grey_600"
+    
+    public var color: UIColor? {
+        let bundle = Bundle(identifier: "kr.mash-up.TwoToo.DesignSystem")
+        return .init(named: self.rawValue, in: bundle, compatibleWith: nil)
+    }
+    
+}
