3d3c07677fbb371b5c7d2761508b78086a6c4146
Ethen <ethen@spoqa.com>
2023-06-16 18:27:37 +0900

    ✅ 닉네임 설정 화면 테스트 코드 작성

--- Scene/NicknameRegistScene/Sources/NicknameRegistScene/NicknameRegistWorker.swift
+++ Scene/NicknameRegistScene/Sources/NicknameRegistScene/NicknameRegistWorker.swift
@@ -8,6 +8,29 @@
 
 import CoreKit
 
-protocol NicknameRegistWorkerProtocol {}
+protocol NicknameRegistWorkerProtocol {
+    /// 초대 유저
+    var invitedUser: NicknameRegist.Model.InvitedUser? { get }
+    /// 닉네임 설정 요청
+    func requestSetNickname(nickname: String) async throws
+    /// 매칭 요청
+    func requestMatching() async throws
+}
 
-final class NicknameRegistWorker: NicknameRegistWorkerProtocol {}
+final class NicknameRegistWorker: NicknameRegistWorkerProtocol {
+    
+    // TODO: UD에서 초대 유저를 꺼내오는 작업 필요
+    var invitedUser: NicknameRegist.Model.InvitedUser? {
+        return nil
+    }
+    
+    // TODO: 닉네임을 설정하는 통신 필요
+    func requestSetNickname(nickname: String) async throws {
+        
+    }
+    
+    // TODO: 초대 유저로 매칭을 요청하는 통신 필요
+    func requestMatching() async throws {
+        
+    }
+}
