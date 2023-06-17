f65f42bc3624cc3fc4895dff8e5857e688bc005d
Ethen <ethen@spoqa.com>
2023-06-17 16:13:12 +0900

    ✅ 로그인 화면 테스트 코드 작성

--- Scene/LoginScene/Sources/LoginScene/LoginInteractor.swift
+++ Scene/LoginScene/Sources/LoginScene/LoginInteractor.swift
@@ -8,9 +8,27 @@
 
 import CoreKit
 
-protocol LoginBusinessLogic {}
+protocol LoginBusinessLogic {
+    /// 첫 진입
+    func didLoad() async
+    /// 카카오 로그인 버튼 클릭
+    func didTapKakaoLoginButton() async
+    /// 애플 로그인 버튼 클릭
+    func didTapAppleLoginButton() async
+    /// 온보딩 스와이프
+    func didSwipeOnboarding(index: Int) async
+}
 
-protocol LoginDataStore: AnyObject {}
+protocol LoginDataStore: AnyObject {
+    /// 닉네임 설정 화면 이동 트리거
+    var didTriggerRouteToNickNameScene: PassthroughSubject<Void, Never> { get }
+    /// 초대장 전송 화면 이동 트리거
+    var didTriggerRouteToInvitationSendScene: PassthroughSubject<Void, Never> { get }
+    /// 대기 화면 이동 트리거
+    var didTriggerRouteToInvitationWaitScene: PassthroughSubject<Void, Never> { get }
+    /// 홈 화면 이동 트리거
+    var didTriggerRouteToHomeScene: PassthroughSubject<Void, Never> { get }
+}
 
 final class LoginInteractor: LoginDataStore, LoginBusinessLogic {
     var cancellables: Set<AnyCancellable> = []
@@ -22,15 +40,30 @@ final class LoginInteractor: LoginDataStore, LoginBusinessLogic {
     init(
         presenter: LoginPresentationLogic,
         router: LoginRoutingLogic,
-        worker: LoginWorkerProtocol
+        worker: LoginWorkerProtocol,
+        didTriggerRouteToNickNameScene: PassthroughSubject<Void, Never>,
+        didTriggerRouteToInvitationSendScene: PassthroughSubject<Void, Never>,
+        didTriggerRouteToInvitationWaitScene: PassthroughSubject<Void, Never>,
+        didTriggerRouteToHomeScene: PassthroughSubject<Void, Never>
     ) {
         self.presenter = presenter
         self.router = router
         self.worker = worker
+        self.didTriggerRouteToNickNameScene = didTriggerRouteToNickNameScene
+        self.didTriggerRouteToInvitationSendScene = didTriggerRouteToInvitationSendScene
+        self.didTriggerRouteToInvitationWaitScene = didTriggerRouteToInvitationWaitScene
+        self.didTriggerRouteToHomeScene = didTriggerRouteToHomeScene
     }
     
     // MARK: - DataStore
     
+    var didTriggerRouteToNickNameScene: PassthroughSubject<Void, Never>
+    
+    var didTriggerRouteToInvitationSendScene: PassthroughSubject<Void, Never>
+    
+    var didTriggerRouteToInvitationWaitScene: PassthroughSubject<Void, Never>
+    
+    var didTriggerRouteToHomeScene: PassthroughSubject<Void, Never>
 }
 
 // MARK: - Interactive Business Logic
@@ -43,10 +76,35 @@ extension LoginInteractor {
     }
 }
 
-// MARK: Feature ()
+// MARK: Feature (진입)
 
 extension LoginInteractor {
     
+    func didLoad() async {
+        
+    }
+}
+
+// MARK: Feature (로그인)
+
+extension LoginInteractor {
+    
+    func didTapKakaoLoginButton() async {
+        
+    }
+    
+    func didTapAppleLoginButton() async {
+        
+    }
+}
+
+// MARK: Feature (온보딩)
+
+extension LoginInteractor {
+    
+    func didSwipeOnboarding(index: Int) async {
+        
+    }
 }
 
 // MARK: - Application Business Logic
