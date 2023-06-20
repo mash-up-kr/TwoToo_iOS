d482c961f283ebf16c30c459ec577312ad871bf3
julia0926 <kj980926@naver.com>
2023-06-20 15:51:05 +0900

    ✨ 공통 네비게이션바 작업

---
+++ TwoToo/NaviTestViewController.swift
@@ -0,0 +1,112 @@
+//
+//  NaviTestViewController.swift
+//  TwoToo
+//
+//  Created by Julia on 2023/06/20.
+//
+
+import CoreKit
+import SceneKit
+import UIKit
+import DesignSystem
+
+public class NaviTestViewController: UIViewController {
+
+    lazy var naviView: TTNavigationBar = {
+        let v = TTNavigationBar(title: "TwoToo",
+                                infoButtonIsHidden: true)
+        v.delegate = self
+        return v
+    }()
+    
+    lazy var navi2View: TTNavigationBar = {
+        let v = TTNavigationBar(title: "마이페이지",
+                                infoButtonIsHidden: false)
+        v.delegate = self
+        return v
+    }()
+    
+    lazy var navi3View: TTNavigationDetailBar = {
+        let v = TTNavigationDetailBar(title: nil,
+                                      infoButtonIsHidden: true)
+        v.delegate = self
+        return v
+    }()
+    
+    lazy var navi4View: TTNavigationDetailBar = {
+        let v = TTNavigationDetailBar(title: "상세페이지",
+                                      infoButtonIsHidden: true)
+        v.delegate = self
+        return v
+    }()
+    
+    lazy var navi5View: TTNavigationDetailBar = {
+        let v = TTNavigationDetailBar(title: "상세페이지2",
+                                      infoButtonIsHidden: false)
+        v.delegate = self
+        return v
+    }()
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .systemBackground
+        
+        self.view.addSubview(self.naviView)
+        self.view.addSubview(self.navi2View)
+        self.view.addSubview(self.navi3View)
+        self.view.addSubview(self.navi4View)
+        self.view.addSubview(self.navi5View)
+
+        self.naviView.snp.makeConstraints { make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
+            make.leading.trailing.equalToSuperview().inset(10)
+            make.height.equalTo(40)
+        }
+        
+        self.navi2View.snp.makeConstraints { make in
+            make.top.equalTo(self.naviView.snp.bottom).offset(20)
+            make.leading.trailing.equalToSuperview().inset(10)
+            make.height.equalTo(40)
+        }
+        
+        self.navi3View.snp.makeConstraints { make in
+            make.top.equalTo(self.navi2View.snp.bottom).offset(20)
+            make.leading.trailing.equalToSuperview().inset(10)
+            make.height.equalTo(40)
+        }
+        
+        self.navi4View.snp.makeConstraints { make in
+            make.top.equalTo(self.navi3View.snp.bottom).offset(20)
+            make.leading.trailing.equalToSuperview().inset(10)
+            make.height.equalTo(40)
+        }
+        
+        self.navi5View.snp.makeConstraints { make in
+            make.top.equalTo(self.navi4View.snp.bottom).offset(20)
+            make.leading.trailing.equalToSuperview().inset(10)
+            make.height.equalTo(40)
+        }
+    }
+}
+
+
+extension NaviTestViewController: TTNavigationBarDelegate{
+    public func tapDetailBackButton() {
+        print("back Detail button")
+    }
+    
+    public func tapDetailRightButton() {
+        print("right Detail button")
+    }
+}
+
+extension NaviTestViewController: TTNavigationDetailBarDelegate{
+    public func tapBackButton() {
+        print("back button")
+    }
+    
+    public func tapRightButton() {
+        print("right button")
+    }
+}
