80d8318d1843de73e78feaceee3e663246437c31
julia0926 <kj980926@naver.com>
2023-06-23 16:54:33 +0900

    ✨ 공통 바텀싯 뷰컨트롤러 작업

---
+++ Core/DesignSystem/Sources/DesignSystem/Sources/Component/BottomSheet/TTBottomSheetViewController.swift
@@ -0,0 +1,80 @@
+//
+//  TTBottomSheetViewController.swift
+//  
+//
+//  Created by Julia on 2023/06/23.
+//
+
+import UIKit
+import FloatingPanel
+
+public protocol TTBottomSheetViewControllerDelegate {
+    
+}
+
+public protocol ScrollableViewController where Self: UIViewController {
+    var scrollView: UIScrollView { get }
+}
+
+final public class TTBottomSheetViewController: FloatingPanelController, TTBottomSheetViewControllerDelegate {
+    
+    private let appearence: SurfaceAppearance = {
+        let v = SurfaceAppearance()
+        v.cornerRadius = 20
+        v.backgroundColor = .mainPink // TODO: 종이 배경으로 변경 필요
+        v.borderColor = .clear
+        v.borderWidth = 0
+        return v
+    }()
+    
+    public init(contentViewController: ScrollableViewController) {
+        super.init(delegate: nil)
+        self.setUpSurfaceView(super.surfaceView)
+        self.setUpBackDropView(super.backdropView)
+        self.setUpView(contentViewController: contentViewController)
+        self.delegate = self
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUpView(contentViewController: ScrollableViewController) {
+        super.set(contentViewController: contentViewController)
+        super.track(scrollView: contentViewController.scrollView)
+        super.layout = TouchBlockIntrinsicPanelLayout()
+    }
+    
+    private func setUpSurfaceView(_ surfaceView: SurfaceView) {
+        surfaceView.grabberHandle.isHidden = false
+        surfaceView.grabberHandle.backgroundColor = .gray
+        surfaceView.grabberHandleSize = .init(width: 40, height: 4)
+        surfaceView.appearance = appearence
+    }
+    
+    private func setUpBackDropView(_ backDropView: BackdropView) {
+        backdropView.dismissalTapGestureRecognizer.isEnabled = false
+        backdropView.backgroundColor = .black
+    }
+    
+}
+
+extension TTBottomSheetViewController: FloatingPanelControllerDelegate {
+    
+    public func floatingPanelDidMove(_ fpc: FloatingPanelController) {
+        let loc = fpc.surfaceLocation
+        let minY = fpc.surfaceLocation(for: .full).y
+        let maxY = fpc.surfaceLocation(for: .tip).y
+        let y = max(min(loc.y, minY), maxY)
+        fpc.surfaceLocation = CGPoint(x: loc.x, y: y)
+    }
+    
+    /// 특정 속도로 아래로 당겼을때 dismiss 되도록 처리됩니다.
+    public func floatingPanelWillEndDragging(_ fpc: FloatingPanelController,
+                                             withVelocity velocity: CGPoint,
+                                             targetState: UnsafeMutablePointer<FloatingPanelState>) {
+        guard velocity.y > 50 else { return }
+        self.dismiss(animated: true)
+    }
+    
+}
