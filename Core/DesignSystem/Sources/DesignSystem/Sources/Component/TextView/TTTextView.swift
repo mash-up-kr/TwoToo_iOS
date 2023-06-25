d1e12b358ec528bf643c403ae2ae250b19e87ee5
julia0926 <kj980926@naver.com>
2023-06-25 13:10:36 +0900

    ✨ 인셋이 있는 기본 텍스트뷰 구현

---
+++ Core/DesignSystem/Sources/DesignSystem/Sources/Component/TextView/TTTextView.swift
@@ -0,0 +1,70 @@
+//
+//  TTTextView.swift
+//  
+//
+//  Created by Julia on 2023/06/25.
+//
+
+import UIKit
+
+public protocol TTTextViewDelegate: AnyObject {
+    func textViewDidEndEditing(text: String)
+}
+
+public final class TTTextView: UITextView {
+    
+    public weak var customDelegate: TTTextViewDelegate?
+    
+    private lazy var placeHolderLabel: UILabel = {
+        let v = UILabel()
+        v.font = .body1
+        v.textColor = .grey600
+        return v
+    }()
+    
+    
+    override init(frame: CGRect, textContainer: NSTextContainer?) {
+        super.init(frame: frame, textContainer: textContainer)
+        self.layout()
+        self.attribute()
+        self.delegate = self
+    }
+    
+    public convenience init(placeHolder: String) {
+        self.init()
+        self.placeHolderLabel.text = placeHolder
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layout() {
+        self.addSubview(self.placeHolderLabel)
+        
+        self.placeHolderLabel.snp.makeConstraints { make in
+            make.top.leading.equalToSuperview().offset(5)
+        }
+    }
+    
+    private func attribute() {
+        self.contentInset = .init(top: 10, left: 10, bottom: 10, right: 10)
+        self.backgroundColor = .mainWhite
+        self.font = .body1
+        self.textColor = .black
+        self.isEditable = true
+    }
+}
+
+extension TTTextView: UITextViewDelegate {
+    public func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.text != nil {
+            self.placeHolderLabel.text = ""
+            textView.textColor = .black
+        }
+    }
+
+    public func textViewDidEndEditing(_ textView: UITextView) {
+        self.customDelegate?.textViewDidEndEditing(text: textView.text)
+    }
+}
