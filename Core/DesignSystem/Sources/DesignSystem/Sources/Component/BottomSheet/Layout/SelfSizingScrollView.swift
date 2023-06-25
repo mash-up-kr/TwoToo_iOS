046ec7145e3f394e82a6f42def7f845ee5b24fcf
julia0926 <kj980926@naver.com>
2023-06-26 00:10:24 +0900

    ♻️ 바텀시트 높이 고정되게 수정

--- Core/DesignSystem/Sources/DesignSystem/Sources/Component/BottomSheet/Layout/SelfSizingScrollView.swift
+++ Core/DesignSystem/Sources/DesignSystem/Sources/Component/BottomSheet/Layout/SelfSizingScrollView.swift
@@ -8,23 +8,15 @@
 import UIKit
 
 final class SelfSizingScrollView: UIScrollView {
-    private let maxHeight: CGFloat
     
     override var intrinsicContentSize: CGSize {
-        CGSize(width: contentSize.width, height: min(contentSize.height, maxHeight))
+        CGSize(width: contentSize.width,
+               height: UIScreen.main.bounds.height * 0.75)
     }
-    
-    init(maxHeight: CGFloat) {
-        self.maxHeight = maxHeight
-        super.init(frame: .zero)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError()
-    }
-    
+
     override func layoutSubviews() {
         super.layoutSubviews()
         invalidateIntrinsicContentSize()
     }
+    
 }
