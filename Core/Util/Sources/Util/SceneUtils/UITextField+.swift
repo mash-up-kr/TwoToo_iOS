f19dd05c015acc8dc701fc4ce00c6faf1e7db236
kyunghoonKim <kimkyunghun3@naver.com>
2023-06-27 14:31:01 +0900

    🚚 UITextField extension Util 패키지로 이동

--- Core/Util/Sources/Util/SceneUtils/UITextField+.swift
+++ Core/Util/Sources/Util/SceneUtils/UITextField+.swift
@@ -1,13 +1,13 @@
 //
-//  UITextFiled+.swift
+//  UITextField+.swift
 //  
 //
-//  Created by Eddy on 2023/06/20.
+//  Created by Eddy on 2023/06/27.
 //
 
 import UIKit
 
-extension UITextField {
+public extension UITextField {
     /// placeholder의 색상을 바꿔주는 메서드
     func setPlaceholder(color: UIColor) {
         guard let string = self.placeholder else {
@@ -17,9 +17,10 @@ extension UITextField {
     }
 
     /// textField에서 글씨를 입력할 때 글자가 입력되는 점의 위치를 조정해주는 메서드
-    public func addLeftPadding(amount: CGFloat) {
+    func addLeftPadding(amount: CGFloat) {
         let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.height))
         self.leftView = paddingView
         self.leftViewMode = .always
     }
 }
+
