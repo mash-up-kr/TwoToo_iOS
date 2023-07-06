cce8e64e14fa52dfdb2bca7d4ee70e3ef731942c
gunoooo <rjsdnqkr1@naver.com>
2023-07-06 22:24:35 +0900

    ✨ 메인 탭바 작업

---
+++ Scene/MainScene/Sources/MainScene/MainTabBarController.swift
@@ -0,0 +1,93 @@
+//
+//  MainTabBarController.swift
+//  TwoToo
+//
+//  Created by 박건우 on 2023/06/04.
+//  Copyright (c) 2023 TwoToo. All rights reserved.
+//
+
+import CoreKit
+import UIKit
+
+protocol MainDisplayLogic: AnyObject {
+}
+
+final class MainTabBarController: UITabBarController {
+    var interactor: MainBusinessLogic
+    
+    init(interactor: MainBusinessLogic) {
+        self.interactor = interactor
+        super.init(nibName: nil, bundle: nil)
+        object_setClass(self.tabBar, MainTabBar.self)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - UI
+    
+    /// 메인 탭바
+    ///
+    /// 기본 탭바 대신 사용합니다.
+    lazy var mainTabBar: MainTabBar = {
+        let v = MainTabBar()
+        v.mainTabBarDelegate = self
+        return v
+    }()
+    
+    // MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setUI()
+        
+        Task {
+            await self.interactor.didLoad()
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        self.view.bringSubviewToFront(self.mainTabBar)
+    }
+    
+    // MARK: - Layout
+    
+    private func setUI() {
+        self.view.backgroundColor = .clear
+        
+        self.view.addSubview(self.mainTabBar)
+        
+        self.mainTabBar.snp.makeConstraints { make in
+            make.edges.equalTo(self.tabBar)
+        }
+    }
+    
+    func setTab(_ tab: Main.ViewModel.MainTab) {
+        self.selectedIndex = tab.rawValue
+        self.mainTabBar.setTab(tab)
+    }
+}
+
+// MARK: - Trigger
+
+extension MainTabBarController: MainTabBarDelegate {
+    
+    func didSelectTab(_ tab: Main.ViewModel.MainTab) {
+        self.selectedIndex = tab.rawValue
+    }
+}
+
+// MARK: - Trigger by Parent Scene
+
+extension MainTabBarController: MainScene {
+    
+}
+
+// MARK: - Display Logic
+
+extension MainTabBarController: MainDisplayLogic {
+    
+}
