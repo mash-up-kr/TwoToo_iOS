249f90d9c25a8675d8e924e3e4af314ee2c6bcfa
julia0926 <kj980926@naver.com>
2023-07-14 19:42:00 +0900

    ♻️ 말풍선 꼬리의 위치에 따라 속성 변경되게 수정

--- Scene/HomeScene/Sources/HomeScene/Views/Flower/SpeechBubble/SpeechBubbleView.swift
+++ Scene/HomeScene/Sources/HomeScene/Views/Flower/SpeechBubble/SpeechBubbleView.swift
@@ -10,47 +10,56 @@ import UIKit
 /// 말풍선 뷰
 /// Parameter
 /// - title : 말풍선 텍스트
-/// - backgroundColor : 말풍선 배경
 /// - tailPosition : 말풍선 꼬리 위치 (좌 / 우)
 final class SpeechBubbleView: UIView {
     
-    private let tipWidth: CGFloat = 10.0
-    private let tipHeight: CGFloat = 6.0
+    private let tipWidth: CGFloat = 12.0
+    private let tipHeight: CGFloat = 8.0
+    private var _backgroundColor: UIColor = .white
+    private var tailCoordinate: CGFloat = 0.5
     
-    enum TailPosition: CGFloat {
-        case left = 0.3
-        case right = 0.7
+    enum TailPosition {
+        case left
+        case right
     }
     
     let titleLabel: UILabel = {
         let v = UILabel()
         v.font = .body1
         v.textColor = .primary
-        v.numberOfLines = 2
+        v.numberOfLines = 0
         return v
     }()
     
     init(title: String,
-         backgroundColor: UIColor,
          tailPosition: TailPosition) {
         super.init(frame: .zero)
         self.layout()
-        self.backgroundColor = backgroundColor
+        switch tailPosition {
+        case .left:
+            self._backgroundColor = .mainLightPink
+            self.tailCoordinate = 0.3
+        case .right:
+            self._backgroundColor = .second01
+            self.tailCoordinate = 0.7
+        }
         self.titleLabel.text = title
-        self.drawTip(bgColor: backgroundColor, tailPosition: tailPosition)
+        self.backgroundColor = _backgroundColor
+        self.drawTip(bgColor: self._backgroundColor,
+                     tailPosition: tailCoordinate)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    func drawTip(bgColor: UIColor, tailPosition: TailPosition) {
+    private func drawTip(bgColor: UIColor, tailPosition: CGFloat) {
         let path = CGMutablePath()
         
         let padding: CGFloat = 10
         let width = self.titleLabel.intrinsicContentSize.width + (padding * 2)
         let height = self.titleLabel.intrinsicContentSize.height + (padding * 2)
-        let tipStartX = width * tailPosition.rawValue
+        let tipStartX = width * tailPosition
 
         let tipWidthCenter = tipWidth / 2.0
         let endXWidth = tipStartX + tipWidth
@@ -63,12 +72,11 @@ final class SpeechBubbleView: UIView {
         let shape = CAShapeLayer()
         shape.path = path
         shape.fillColor = bgColor.cgColor
-
+        
         self.layer.insertSublayer(shape, at: 0)
         self.layer.masksToBounds = false
         self.layer.cornerRadius = 8
     }
-
     
     func layout() {
         self.addSubview(self.titleLabel)
