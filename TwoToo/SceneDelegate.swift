f6d7b290705d25692d64f33505c44cc5e1654048
gunoooo <rjsdnqkr1@naver.com>
2023-07-24 21:57:34 +0900

    🐛 develop 충돌 해결

--- TwoToo/SceneDelegate.swift
+++ TwoToo/SceneDelegate.swift
@@ -60,8 +60,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         self.window?.rootViewController = nav
     }
     
-    
-
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
 //        if let url = URLContexts.first?.url {
 //            if (AuthApi.isKakaoTalkLoginUrl(url)) {
@@ -70,7 +68,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 //        }
     }
 
->>>>>>> develop
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
