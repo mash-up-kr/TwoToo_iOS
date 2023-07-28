3ce67873627da77086d5818b801262461007700f
kyunghoonKim <kimkyunghun3@naver.com>
2023-07-28 14:42:31 +0900

    ✨ 마이페이지 UI 구현

--- Scene/MyInfoScene/Sources/MyInfoScene/MyInfoViewController.swift
+++ Scene/MyInfoScene/Sources/MyInfoScene/MyInfoViewController.swift
@@ -14,6 +14,55 @@ protocol MyInfoDisplayLogic: AnyObject {}
 final class MyInfoViewController: UIViewController {
     var interactor: MyInfoBusinessLogic
     
+    // MARK: - UI
+    
+    private lazy var navigationBar: TTNavigationBar = {
+        let v = TTNavigationBar(title: "마이페이지", rightButtonImage: .asset(.icon_info))
+        return v
+    }()
+    
+    private lazy var mainImageView: UIImageView = {
+        let v = UIImageView()
+        v.image = .asset(.icon_nicknam_my)
+        return v
+    }()
+    
+    private lazy var coupleLabel: UILabel = {
+        let v = UILabel()
+        v.text = "공주 ❤️ 왕자"
+        v.textColor = .mainCoral
+        v.font = .body3
+        return v
+    }()
+    
+    private lazy var challengeCountLabel: UILabel = {
+        let v = UILabel()
+        v.textColor = .mainCoral
+        v.font = .body3
+        v.text = "1번째 꽃 피우는중"
+        return v
+    }()
+    
+    private lazy var myNameTagView: TTTagView = {
+        let v = TTTagView(textColor: .primary, fontSize: .body2, cornerRadius: 15)
+        v.titleLabel.text = "공주"
+        return v
+    }()
+    
+    private lazy var separator: TTSeparator = {
+        let v = TTSeparator(color: .second01, height: 3)
+        return v
+    }()
+    
+    private lazy var tableView: UITableView = {
+        let v = UITableView(frame: .zero)
+        v.registerCell(MyInfoTableViewCell.self)
+        v.backgroundColor = .second02
+        v.dataSource = self
+        v.separatorStyle = .none
+        return v
+    }()
+    
     init(interactor: MyInfoBusinessLogic) {
         self.interactor = interactor
         super.init(nibName: nil, bundle: nil)
@@ -22,8 +71,6 @@ final class MyInfoViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    // MARK: - UI
     
     // MARK: - View Lifecycle
     
@@ -35,7 +82,51 @@ final class MyInfoViewController: UIViewController {
     // MARK: - Layout
     
     private func setUI() {
+        self.view.backgroundColor = .second02
         
+        self.view.addSubviews(
+            self.navigationBar, self.mainImageView,
+            self.coupleLabel, self.challengeCountLabel,
+            self.myNameTagView, self.separator,
+            self.tableView
+        )
+        
+        self.navigationBar.snp.makeConstraints { make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(44)
+        }
+        
+        self.mainImageView.snp.makeConstraints { make in
+            make.top.equalTo(self.navigationBar.snp.bottom)
+            make.leading.trailing.equalToSuperview().inset(113)
+            make.height.equalTo(UIScreen.main.bounds.height * 0.158)
+        }
+        
+        self.coupleLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.mainImageView.snp.bottom).offset(19)
+            make.centerX.equalTo(self.mainImageView.snp.centerX)
+        }
+        
+        self.challengeCountLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.coupleLabel.snp.bottom).offset(10)
+            make.centerX.equalTo(self.mainImageView.snp.centerX)
+        }
+        
+        self.myNameTagView.snp.makeConstraints { make in
+            make.top.equalTo(self.challengeCountLabel.snp.bottom).offset(14)
+            make.centerX.equalTo(self.mainImageView.snp.centerX)
+        }
+        
+        self.separator.snp.makeConstraints { make in
+            make.top.equalTo(self.myNameTagView.snp.bottom).offset(34)
+            make.leading.trailing.equalToSuperview()
+        }
+        
+        self.tableView.snp.makeConstraints { make in
+            make.top.equalTo(self.separator.snp.bottom).offset(11)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
     }
 }
 
@@ -51,4 +142,15 @@ extension MyInfoViewController: MyInfoScene {
 
 extension MyInfoViewController: MyInfoDisplayLogic {
     
+}
+
+extension MyInfoViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueCell(type: MyInfoTableViewCell.self, indexPath: indexPath)
+        return cell
+    }
 }
