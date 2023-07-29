85f1bf24d7235b1b3fe73141e7934536cf9dc734
kimkyunghun3 <kh.kim@lifesemantics.kr>
2023-07-30 03:15:11 +0900

    ✨ Formatter 케이스 추가

--- Core/Util/Sources/Util/Constants/DateFormatType.swift
+++ Core/Util/Sources/Util/Constants/DateFormatType.swift
@@ -12,6 +12,7 @@ public enum DateFormatType: String {
     case yearMonthDay = "YYYY/MM/dd"
     case monthDay = "MM/dd"
     case hourMinute = "HH:mm"
+    case iso = "yyyy-MM-dd'T'HH:mm:ss.SSZ"
 
     var displayName: String {
         return self.rawValue
