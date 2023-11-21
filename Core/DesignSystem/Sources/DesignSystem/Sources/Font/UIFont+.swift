665fdc650f11e955ca302ad4b3a33c50091a66bb
julia0926 <kj980926@naver.com>
2023-11-21 10:28:48 +0900

    ✨ 나가기 버튼, 말풍선 UI 구현
    - 말풍선 폴더 위치 변경 (홈 -> 디자인시스템)

--- Core/DesignSystem/Sources/DesignSystem/Sources/Font/UIFont+.swift
+++ Core/DesignSystem/Sources/DesignSystem/Sources/Font/UIFont+.swift
@@ -9,30 +9,31 @@ import UIKit
 
 extension UIFont {
     
+    /// fontSize : 28
     public static var h1: UIFont {
         return .omyupretty(size: ._28)
     }
-    
+    /// fontSize : 24
     public static var h2: UIFont {
         return .omyupretty(size: ._24)
     }
-    
+    /// fontSize : 20
     public static var h3: UIFont {
         return .omyupretty(size: ._20)
     }
-    
+    /// fontSize : 18
     public static var h4: UIFont {
         return .omyupretty(size: ._18)
     }
-    
+    /// fontSize : 16
     public static var body1: UIFont {
         return .omyupretty(size: ._16)
     }
-    
+    /// fontSize : 15
     public static var body2: UIFont {
         return .omyupretty(size: ._15)
     }
-    
+    /// fontSize : 12
     public static var body3: UIFont {
         return .omyupretty(size: ._12)
     }
